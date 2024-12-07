b71b91bba0b789487c6fb2e27999d41742d5f01c
Valerio Lomanto <gv.lomanto@gmail.com>
2024-12-07 16:07:54 +0100

    solve day7

--- aoc/2024/day07/src/main.rs
+++ aoc/2024/day07/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 
 use utils::get_stdinput;
 
@@ -9,15 +10,70 @@ fn main() {
     let p2 = solve2(&parsed);
     println!("sol2: {p2:?}");
 }
-type Input = ();
+type Input = Vec<Calib>;
+type Calib = (isize, Vec<isize>);
 
 fn parse(lines: impl Iterator<Item = impl AsRef<str>>) -> Input {
-    lines;
+    lines
+        .filter(|s| !s.as_ref().trim().is_empty())
+        .map(parse_calib)
+        .collect()
 }
 
-fn solve1(input: &Input) -> () {}
+fn parse_calib(s: impl AsRef<str>) -> Calib {
+    let (target, operands) = s.as_ref().split_once(":").expect("should have a space");
+    let target = target.trim().parse().expect("should be a number");
+    let operands = operands
+        .trim()
+        .split_whitespace()
+        .map(|n| n.parse().expect("should be a number"))
+        .collect();
+    (target, operands)
+}
 
-fn solve2(input: &Input) -> () {}
+fn solve1(input: &Input) -> isize {
+    input
+        .iter()
+        .filter_map(|(target, ops)| possibilities(ops).contains(target).then_some(target))
+        .sum()
+}
+
+fn possibilities(ops: &[isize]) -> HashSet<isize> {
+    let mut ops = ops.iter().copied();
+    let fst = ops.next().unwrap();
+    ops.fold(HashSet::from([fst]), |set, n| {
+        set.into_iter()
+            .flat_map(|m| [m + n, m * n].into_iter())
+            .collect()
+    })
+}
+
+fn concate(a: isize, b: isize) -> isize {
+    let mut x = b;
+    let mut pow = 1;
+    while x > 0 {
+        pow *= 10;
+        x /= 10;
+    }
+    a * pow + b
+}
+
+fn more_possibilities(ops: &[isize]) -> HashSet<isize> {
+    let mut ops = ops.iter().copied();
+    let fst = ops.next().unwrap();
+    ops.fold(HashSet::from([fst]), |set, n| {
+        set.into_iter()
+            .flat_map(|m| [m + n, m * n, concate(m, n)].into_iter())
+            .collect()
+    })
+}
+
+fn solve2(input: &Input) -> isize {
+    input
+        .iter()
+        .filter_map(|(target, ops)| more_possibilities(ops).contains(target).then_some(target))
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -27,16 +83,13 @@ mod tests {
     fn test1() {
         let input = include_str!("../test");
         let input = parse(input.lines());
-        assert_eq!(solve1(&input), ());
+        assert_eq!(solve1(&input), 3749);
     }
 
     #[test]
     fn test2() {
         let input = include_str!("../test");
         let input = parse(input.lines());
-        assert_eq!(solve2(&input), ());
+        assert_eq!(solve2(&input), 11387);
     }
 }
-
-
-    
