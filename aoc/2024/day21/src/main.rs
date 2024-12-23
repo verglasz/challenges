d4a27a922108ec95957a9ed3438564f7491ab6fe
Valerio Lomanto <gv.lomanto@gmail.com>
2024-12-24 00:30:03 +0100

    init day21

---
+++ aoc/2024/day21/src/main.rs
@@ -0,0 +1,42 @@
+use utils::get_stdinput;
+
+fn main() {
+    let input: Vec<_> = get_stdinput().collect();
+    let parsed = parse(input.iter().map(|x| x.as_str()));
+    let p1 = solve1(&parsed);
+    println!("sol1: {p1}");
+    let p2 = solve2(&parsed);
+    println!("sol2: {p2}");
+}
+type Input = ();
+
+fn parse<'a>(lines: impl Iterator<Item = &'a str>) -> Input {
+    lines;
+}
+
+fn solve1(input: &Input) -> usize {
+    0
+}
+
+fn solve2(input: &Input) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let input = include_str!("../test");
+        let input = parse(input.lines());
+        assert_eq!(solve1(&input), 0);
+    }
+
+    #[test]
+    fn test2() {
+        let input = include_str!("../test");
+        let input = parse(input.lines());
+        assert_eq!(solve2(&input), 0);
+    }
+}
