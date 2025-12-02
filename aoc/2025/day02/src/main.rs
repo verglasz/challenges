6d6307229f3fae38eb4b743a4942cde9a0d7d0c9
Valerio Lomanto <gv.lomanto@gmail.com>
2025-12-02 10:13:29 +0100

    day2 init ish

---
+++ aoc/2025/day02/src/main.rs
@@ -0,0 +1,140 @@
+use std::{collections::HashSet, ops::RangeInclusive};
+
+use utils::{
+    decimals::{digits, mask10, next_pow10},
+    get_stdinput,
+};
+
+fn main() {
+    let input: Vec<_> = get_stdinput().collect();
+    let parsed = parse(&input[0]);
+    let p1 = solve1(&parsed);
+    println!("sol1: {p1}");
+    let p2 = solve2(&parsed);
+    println!("sol2: {p2}");
+}
+type Input<'a> = Vec<RangeInclusive<&'a str>>;
+
+fn parse<'a>(line: &str) -> Input<'_> {
+    line.split(',')
+        .map(|range| range.split_once('-').expect("should contain -"))
+        .map(|(a, b)| a..=b)
+        .collect()
+}
+
+fn solve1(input: &Input) -> usize {
+    let mut total = 0;
+    for range in input {
+        let first = find_rep2_above(range.start(), false);
+        let next_last = find_rep2_above(range.end(), true);
+        for id in first..next_last {
+            total += make_id(id, 2);
+        }
+    }
+    total
+}
+
+fn make_id(base: usize, reps: usize) -> usize {
+    let digs = mask10(base);
+    let mut total = 1;
+    for _ in 0..(reps - 1) {
+        total = total * digs + 1;
+    }
+    total * base
+}
+
+fn find_rep2_above(start: &str, strict: bool) -> usize {
+    assert!(start.len() > 0);
+    if start.len() % 2 != 0 {
+        10_usize.pow((start.len() / 2) as u32)
+    } else {
+        let (top, bot) = start.split_at(start.len() / 2);
+        let top = top.parse::<usize>().expect("numbers in ids");
+        let bot = bot.parse::<usize>().expect("numbers in ids");
+        if top > bot {
+            top
+        } else if top < bot {
+            top + 1
+        } else {
+            // top == bot, if strict we still increment
+            top + strict as usize
+        }
+    }
+}
+
+// fn find_last(end: &str) -> usize { todo!() }
+
+fn solve2(input: &Input) -> usize {
+    let mut total = 0;
+    let mut ids = HashSet::new();
+    for range in input {
+        println!("range {range:?}");
+        let first = (find_rep_n_above(range.start(), false, range.end()));
+        let next_last = (find_rep_n_above(range.end(), true, range.end()));
+        for (f, l) in first.iter().zip(next_last) {
+            println!("f:{f:?} l:{l:?}");
+            let fa = f.1;
+            let la = l.1;
+            assert!(f.0 == l.0);
+            for id in fa..la {
+                ids.insert(dbg!(make_id(id, f.0)));
+            }
+        }
+        total += ids.drain().sum::<usize>();
+    }
+    total
+}
+
+/// return a vector of repeat_times, base_id
+fn find_rep_n_above(start: &str, strict: bool, stop: &str) -> Vec<(usize, usize)> {
+    assert!(start.len() > 0);
+    let mut all = vec![];
+    let max_reps = stop.len();
+    for reps in 2..=max_reps {
+        let digs = start.len() / reps;
+        let num = if start.len() % reps != 0 {
+            10_usize.pow(digs as u32)
+        } else {
+            let mut chks = start.as_bytes().chunks(digs).map(|c| {
+                str::from_utf8(c)
+                    .expect("was ascii nums so no broken chars")
+                    .parse::<usize>()
+                    .expect("number")
+            });
+            let fst = chks.next().expect("at least one piece");
+            // we need to go above fst if any subsequent digit group is above
+            // or if they're all exactly equal
+            let mut increment = strict;
+            for c in chks {
+                increment = match c.cmp(&fst) {
+                    std::cmp::Ordering::Greater => true,
+                    std::cmp::Ordering::Equal => continue,
+                    std::cmp::Ordering::Less => false,
+                };
+                break;
+            }
+            fst + increment as usize
+        };
+        all.push((reps, num))
+    }
+    all
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let input = include_str!("../test");
+        let input = parse(input.lines().next().unwrap());
+        assert_eq!(solve1(&input), 1227775554);
+    }
+
+    #[test]
+    fn test2() {
+        let input = include_str!("../test");
+        let input = parse(input.lines().next().unwrap());
+        assert_eq!(solve2(&input), 4174379265);
+    }
+}
--- aoc/2025/utils/src/decimals.rs
+++ aoc/2025/utils/src/decimals.rs
@@ -1,6 +1,6 @@
+/// return a power of 10 which covers n, as in, has as many zeros as n has digits
+/// eg 123 -> 1000, 1234 -> 10000, 10 -> 100
 pub fn mask10(n: usize) -> usize {
-    // a power of 10 which covers n, as in, has as many zeros as n has digits
-    // eg 123 -> 1000, 1234 -> 10000
     let mut n = n;
     let mut pow = 1;
     while n > 0 {
@@ -10,7 +10,33 @@ pub fn mask10(n: usize) -> usize {
     pow
 }
 
+/// return a power of 10 which  is >= n
+/// eg 123 -> 1000, 1234 -> 10000, 10 -> 10
+pub fn next_pow10(n: usize) -> usize {
+    let mut n = n;
+    let mut pow = 1;
+    let mut rem = false;
+    while n > 1 || (n > 0 && rem) {
+        pow *= 10;
+        rem |= n % 10 > 0;
+        n /= 10;
+    }
+    pow
+}
+
+/// Return the number of decimal digits of the number, eg:
+/// ```
+/// use utils::decimals::digits;
+/// assert!(digits(0) == 1);
+/// assert!(digits(3) == 1);
+/// assert!(digits(233) == 3);
+/// assert!(digits(1000) == 4);
+/// assert!(digits(9999) == 4);
+/// ```
 pub fn digits(n: usize) -> usize {
+    if n == 0 {
+        return 1;
+    }
     let mut n = n;
     let mut digits = 0;
     while n > 0 {
@@ -19,3 +45,6 @@ pub fn digits(n: usize) -> usize {
     }
     digits
 }
+
+#[cfg(test)]
+mod tests {}
